3cdd4280569b1972c90acb53fbc940393e04c73d
Damilola Adekoya <dharmykoya38@gmail.com>
2022-11-26 12:40:19 +0100

    persist redux

--- src/index.js
+++ src/index.js
@@ -7,17 +7,31 @@ import reportWebVitals from "./reportWebVitals";
 import { createStore } from "redux";
 import rootReducer from "./Reducers";
 import { Provider } from "react-redux";
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import { PersistGate } from 'redux-persist/integration/react';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = createStore(
-  rootReducer,
+  persistedReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+const persist_store = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persist_store}>
+      <App />
+    </PersistGate>
   </Provider>
 
   // </React.StrictMode>
